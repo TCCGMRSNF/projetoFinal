d746080eb25bc07ea4aebc7e8850ed8462772400
KaizukaDev <snfdev@gmail.com>
2020-05-19 21:36:05 -0300

    Query genérica para eventos

--- selector/src/routes/eventos.js
+++ selector/src/routes/eventos.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
-router.get('/', isLoggedIn, async (req, res) => {
-    const eventos = await pool.query('SELECT * FROM eventos WHERE owner = ?', [req.user.id]);
+router.get('/:funcao', isLoggedIn, async (req, res) => {
+    const { funcao } = req.params;
+    sFuncao = funcao.toString();
+    console.log('H'+funcao+'H');
+    const eventos = await pool.query(
+        'SELECT * FROM eventos WHERE id IN(SELECT evt_id FROM evento_usuario WHERE usr_id = ? AND funcao = ?)'
+        , [req.user.id, sFuncao]);
     console.log(eventos);
     res.render('eventos/eventos_list', { eventos });
 });
