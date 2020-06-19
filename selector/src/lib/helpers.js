2e12e5a7857dcbfcd3e42a4a07d52a9344668a21
KaizukaDev <snfdev@gmail.com>
2020-06-18 23:19:03 -0300

    Página Avalidor 14

--- selector/src/lib/helpers.js
+++ selector/src/lib/helpers.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const helpers = {};
 const faker = require('faker');
+const pool = require('../database');
 
 helpers.encryptPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
@@ -16,17 +17,16 @@ helpers.matchPassword = async (password, savedPassword) => {
     }
 };
 
-
 helpers.fakeUser = (locale) => {
     faker.locale = locale;
     const fakeUser = {
         nome: faker.name.findName(),
         email: faker.internet.email(),
         senha: '1234567890'
-//        website: faker.internet.url(),
-//        address: faker.address.streetAddress() + faker.address.city() + faker.address.country(),
-//        bio: faker.lorem.sentences(),
-//        image: faker.image.avatar()
+        //        website: faker.internet.url(),
+        //        address: faker.address.streetAddress() + faker.address.city() + faker.address.country(),
+        //        bio: faker.lorem.sentences(),
+        //        image: faker.image.avatar()
     }
     return fakeUser;
 }
@@ -36,6 +36,25 @@ helpers.isAdm = (adm) => {
     return adm === 'Y';
 };
 */
+
+helpers.getQuesitos = async (evtId) => {
+    const quesitos = await pool.query(
+        'SELECT * FROM quesitos AS ques, evento_quesito AS eq WHERE eq.evt_id = ? AND eq.qst_id = ques.id'
+        , [evtId]);
+    return (quesitos);
+}
+
+helpers.getCandidatos = async (evtId) => {
+    const candidatos = await pool.query(
+        'SELECT * FROM evento_usuario AS eu WHERE eu.evt_id = ? AND eu.funcao = "2"'
+        , [evtId]);
+    return (candidatos);
+}
+
+
+
+
+
 
 
 
--- selector/src/routes/notas.js
+++ selector/src/routes/notas.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const helpers = require('../lib/helpers');
 
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
-router.post('/:evtId/:quesId/:canId', isLoggedIn, async (req, res) => {
+router.post('/gravar/:evtId/:quesId/:canId', isLoggedIn, async (req, res) => {
     const { evtId, quesId, canId} = req.params;
     const nCanNota = req.body.inputNota;
     const sAvlId = req.user.id.toString();
@@ -19,5 +20,40 @@ router.post('/:evtId/:quesId/:canId', isLoggedIn, async (req, res) => {
     res.redirect(backURL);
     //res.status(200);
 });
+
+router.get('/grid/:evtId', isLoggedIn, async (req, res) => {
+    const { evtId } = req.params;
+    const sAvlId = req.user.id.toString();
+    const sEvtId = evtId.toString();
+
+    const quesitos = await helpers.getQuesitos(evtId);
+    const candidatos = await helpers.getCandidatos(evtId);
+    
+    console.log(quesitos, candidatos, '=============================');
+
+
+
+
+
+    backURL=req.header('Referer') || '/';
+    res.redirect(backURL);
+    //res.status(200);
+});
+
+/*
+async function getQuesitos(evtId) {
+    const quesitos = await pool.query(
+        'SELECT * FROM quesitos AS ques, evento_quesito AS eq WHERE eq.evt_id = ? AND eq.qst_id = ques.id'
+        , [evtId]);
+    return (quesitos);
+}
+*/
+
+
+
+
+
+
+
 
 module.exports = router;
