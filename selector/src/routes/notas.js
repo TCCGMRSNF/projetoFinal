a27002a1d965c31c62582e13c5c3b1968a439ddc
kaizuka <snfdev@gmail.com>
2020-07-05 13:15:02 -0300

    Ajustes 01

--- selector/src/routes/notas.js
+++ selector/src/routes/notas.js
@@ -5,6 +5,7 @@ const helpers = require('../lib/helpers');
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
+// Gravar nota do candidato para evento+quesito selecionado.
 router.post('/gravar/:evtId/:quesId/:canId', isLoggedIn, async (req, res) => {
     const { evtId, quesId, canId } = req.params;
     const nCanNota = req.body.inputNota;
@@ -21,9 +22,10 @@ router.post('/gravar/:evtId/:quesId/:canId', isLoggedIn, async (req, res) => {
     //res.status(200);
 });
 
+// Gerador de grid de notas zeradas. 
+// Não gera nota se já possui nota lançada.
 router.get('/grid/:evtId', isLoggedIn, async (req, res) => {
     const { evtId } = req.params;
-    //    const sAvlId = req.user.id.toString();
     const sEvtId = evtId.toString();
     const quesitos = await helpers.getQuesitos(evtId);
     const candidatos = await helpers.getCandidatos(evtId);
@@ -42,11 +44,12 @@ router.get('/grid/:evtId', isLoggedIn, async (req, res) => {
                     evt_id: evtId,
                     avl_id: aval.usr_id,
                     cdt_id: can.usr_id,
-                    nota00: 0, nota01: 0, nota02:0, nota03: 0, nota04: 0,
-                    nota05: 0, nota06: 0, nota07:0, nota08: 0, nota09: 0
+                    numero: can.numero,
+                    nota00: 0, nota01: 0, nota02: 0, nota03: 0, nota04: 0,
+                    nota05: 0, nota06: 0, nota07: 0, nota08: 0, nota09: 0
                 };
                 await pool.query(
-                   'INSERT INTO notas SET ?', [aNota]);
+                    'INSERT INTO notas SET ?', [aNota]);
             };
         });
     });
@@ -54,6 +57,50 @@ router.get('/grid/:evtId', isLoggedIn, async (req, res) => {
     res.redirect(backURL);
     //res.status(200);
 });
+/*
+// Cálculo de médias e escores
+router.get('/scores/:evtId', isLoggedIn, async (req, res) => {
+    const { evtId } = req.params;
+    const sEvtId = evtId.toString();
+    const quesitos = await helpers.getQuesitos(evtId);
+    const candidatos = await helpers.getCandidatos(evtId);
+    //    const avaliadores = await helpers.getAvaliadores(evtId);
+
+    console.log(quesitos);
+    console.log(candidatos);
+    //  console.log(avaliadores);
+
+
+
+    candidatos.forEach((can) => {
+        var sCanId = can.usr_id.toString();
+        var rows = await pool.query(
+            'SELECT AVG(nota00), AVG(nota01), AVG(nota02), AVG(nota03), AVG(nota04), AVG(nota05), AVG(nota06), AVG(nota07), AVG(nota08), AVG(nota09) FROM notas WHERE evt_id = ? AND cdt_id = ?'
+            , [sEvtId, sAvlId, sCanId]);
+
+        if (rows.length === 0) {
+            var aNota = {
+                evt_id: evtId,
+                avl_id: aval.usr_id,
+                cdt_id: can.usr_id,
+                nota00: 0, nota01: 0, nota02: 0, nota03: 0, nota04: 0,
+                nota05: 0, nota06: 0, nota07: 0, nota08: 0, nota09: 0
+            };
+            await pool.query(
+                'INSERT INTO notas SET ?', [aNota]);
+        };
+
+    });
+
+    backURL = req.header('Referer') || '/';
+    res.redirect(backURL);
+    //res.status(200);
+});
+
+*/
+
+
+
 
 
 
